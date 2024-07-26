2f93de501c337b2b464a09449130031e71244807
hdoan234 <hdoan5619@gmail.com>
2024-07-26 00:02:20 -0500

    fixed home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import Cube from './Cube.js';
+import {AnimatePresence, motion} from 'framer-motion';
 
+const variants = {
+  initial:{
+    x:100,
+    opacity:0
+  },
+  animate:{
+    x:0,
+    opacity:1
+  },
+  exit:{
+    x:-100,
+    opacity:0
+  }
+}
 function App() {
   function prevStep(){
     if(index === 0){
@@ -21,7 +35,7 @@ function App() {
   }
   const[index, setIndex] = React.useState(0);
 
-  const title = "HandScript";
+  const title = "SignScript";
   const image=[
     "https://i.pinimg.com/564x/c2/86/44/c28644061060cb6f04e17f6e9e7870fe.jpg",
     "https://i.pinimg.com/564x/5e/31/d5/5e31d59d9c92b8ee4d67d095d9beed3c.jpg",
@@ -29,29 +43,28 @@ function App() {
     "https://i.pinimg.com/564x/02/1a/65/021a65b9aedb22ad9482babb9925b5bd.jpg"
     
   ]
+
   return (
     <div className="App">
-       <div className='header-bar'>
-          <p>Home</p>
-          <p>About</p>
-          <p>Practice</p>
-          <p>Contact</p>
-        </div>
         <div className='background'>
           <div className='container1'>
             <div>
               <h1 className='header'>{title}</h1>
               <p className='slogan'>Sign Language, Simplified</p>
             </div>   
-        <div className='slideshow'>
-          <img src={image[index]} alt="Sign Language"/>
-          <button className="prev" onClick={prevStep}> prev</button>
-          <button className="next" onClick={nextStep}>next</button>
+            <div className='slideshow'>
+              <AnimatePresence initial={false}>
+                <motion.img variants={variants} animate="animate" initial="initial" exit="exit" src={image[index]} alt="Sign Language" className='slide' key={image[index]} />
+              </AnimatePresence>  
+              <button className="prev" onClick={prevStep}>&lt; </button>
+              <button className="next" onClick={nextStep}>&gt;</button>
+            </div>
+          <div className='buttons'>
+            <button className='transcript-button'>Start Transcript</button>
+            <button className='learn-button'>Learn More</button>
+          </div>
+          
         </div>
-        <button className='transcript-button'>Start Transcript</button>
-        
-        {/* <Cube /> */}
-      </div>
       </div>
       
     </div>
